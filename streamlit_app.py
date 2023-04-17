8e686b4705abfda6bb664c11627215899dd09623
amsf741 <130979099+amsf741@users.noreply.github.com>
2023-04-17 13:01:12 +0100

    Added few code lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
+streamlit.header('Date Engineering success path')
 streamlit.title('Hello World - bravo Alagappan')
+streamlit.text('Master SQL')
+streamlit.text('Master Snowflake')
+streamlit.text('Get certification')
+streamlit.text('Get a good job')
